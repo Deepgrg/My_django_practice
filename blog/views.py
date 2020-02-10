a350860486b22d0a24674b9c1c51e1325c2370e3
Dip Gurung <deepgrg03@gmail.com>
2020-02-10 14:02:32 +0545

    Added some design to the page

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
 
+from posts.models import Posts
 from try_django.form import ContactForm
 
 def home_page(request):
+    qs = Posts.objects.all()[:4]
     context = {
-        'title' : 'Non user data' ,
-         
+        'title' : 'Welcome' ,
+        'posts' : qs,
         }
-    if request.user.is_authenticated :
-        context = {
-        'title' : 'User data' ,
-         'list' : [1,2,3,5,6,8],
-        }     
+       
     return render(request , 'blog/home_page.html' ,context)
 
 
