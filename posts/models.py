021864275e257d99af2e85a43fbeceaa96f5888e
Dip Gurung <deepgrg03@gmail.com>
2020-02-11 17:57:02 +0545

    Completed till the date and time of posts

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
+class PostsQuerySet(models.QuerySet):
+    def published(self):
+        now=timezone.now()
+        return self.filter(publish_date__lte=now)
+
+class PostsManager(models.Manager):
+        def get_queryset(self):
+            return PostsQuerySet(self.model , using=self._db)
+        
+        def published(self):
+            return self.get_queryset().published()
 
 class Posts(models.Model):
     user = models.ForeignKey(User , on_delete=models.CASCADE , default = 1)
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique = True )
     content = models.TextField(null=True , blank = True)
+    publish_date = models.DateTimeField(auto_now=False , auto_now_add=False, null=True , blank = True)
+    timestamp = models.DateTimeField( auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects=PostsManager()
+    class Meta:
+        ordering = ['-publish_date' , '-updated' , '-timestamp']
+
 
     def __str__(self):
         return self.title
